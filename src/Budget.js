999c2d2db723d106f5eef3cfaa9a2eba4f6b5af2
Daniel Oquinn <danieljoquinn@gmail.com>
2019-09-30 03:14:09 -0400

    Balance budget

--- src/Budget.js
+++ src/Budget.js
@@ -7,13 +7,14 @@ class Budget extends Component {
       <div className="Budget">
       Budget <br />
       Income Streams: <br />
-      Paycheck $2000 2/month
+      Paycheck: $2000 2/month <br>
+      Alcohol, record & bag sales profit: $400/mo <br>
+
 
       Expenses:<br />
       Mortgage: $1300 1/month <br />
       Internet: $60 1/month<br />
       Phone: $120 1/month<br />
-      Alcohol: $400 1/month<br />
 
 
       </div>
